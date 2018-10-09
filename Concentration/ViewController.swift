b14326a912417c377ea5f478bc74978cb854714b
Mike JS. Choi <mkchoi212@icloud.com>
2018-10-09 14:31:22 -0500

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Mike Choi on 10/9/18.
+//  Copyright © 2018 Mike JS. Choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
